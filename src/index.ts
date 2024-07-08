0db44dcf16b6be27649c0318b1d9f74b10bed7f4
Jacob Stanley <>
2024-07-08 22:58:09 +0100

    initial commit, working

---
+++ src/index.ts
@@ -0,0 +1,78 @@
+// src/index.ts
+import { Ableton } from "ableton-js";
+import OBSWebSocket from "obs-websocket-js"
+import { TimeFormat } from "ableton-js/ns/song";
+
+const SYNC_THRESHOLD = 2000;
+
+const ableton = new Ableton({ logger: console });
+const obs = new OBSWebSocket();
+const videos = ["BackgroundVideo", "ForegroundVideo"];
+// TODO: args
+
+const synchronise = async (obs: OBSWebSocket) => {
+
+    const msTime = await ableton.song.getCurrentSmpteSongTime(TimeFormat.MsTime);
+    const msTimeLong = msTime.frames + (msTime.seconds * 1000) + (msTime.minutes * 60 * 1000) + (msTime.hours * 60 * 60 * 1000);
+
+    const videoTimeBg = await obs.call("GetMediaInputStatus", {
+        inputName: videos[0]
+    });
+    const videoTimeFg = await obs.call("GetMediaInputStatus", {
+        inputName: videos[1]
+    });
+
+    if (Math.abs(videoTimeBg.mediaCursor - msTimeLong) > SYNC_THRESHOLD || Math.abs(videoTimeFg.mediaCursor - msTimeLong) > SYNC_THRESHOLD) {
+        console.log(`At least one video is out of sync by ${SYNC_THRESHOLD}ms. Synchronising to Ableton (${msTimeLong}ms)`);
+        for (const video of videos) {
+            obs
+                .call("TriggerMediaInputAction", {
+                    inputName: video,
+                    mediaAction: "OBS_WEBSOCKET_MEDIA_INPUT_ACTION_PAUSE"
+                })
+                .then(() => {
+                    obs.call("SetMediaInputCursor", {
+                        inputName: video,
+                        mediaCursor: msTimeLong
+                    })
+                .then(() => {
+                    obs.call("TriggerMediaInputAction", {
+                        inputName: video,
+                        mediaAction: "OBS_WEBSOCKET_MEDIA_INPUT_ACTION_PLAY"
+                    });
+                })
+            });
+        }
+    }
+};
+
+const pause = async(is_playing: boolean, obs: OBSWebSocket) => {
+    console.log("Playback " + (is_playing ? "started" : "stopped"))
+    for (const video of videos) {
+        obs.call("TriggerMediaInputAction", {
+            inputName: video,
+            mediaAction: is_playing ? "OBS_WEBSOCKET_MEDIA_INPUT_ACTION_PLAY" : "OBS_WEBSOCKET_MEDIA_INPUT_ACTION_PAUSE"
+        });
+        if (is_playing) {
+            synchronise(obs);
+        }
+    }
+};
+
+const start = async () => {
+    // Establishes a connection with Live
+    await ableton.start();
+    await obs.connect("ws://127.0.0.1:4455");
+
+    // stop any playback if we're not playing on launch
+    pause(await ableton.song.get("is_playing"), obs)
+
+    // observe the current playback state
+    ableton.song.addListener("is_playing", (is_playing) => pause(is_playing, obs));
+
+    // TODO: pause when Ableton stops, maybe via callback?
+    // synchronise every 5 seconds
+    setInterval(() => synchronise(obs), 5000);
+};
+
+start();
